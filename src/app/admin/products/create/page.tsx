d6923b90c3758204d6ef662901f4fa6dc4d49ca1
NikDevRych <nikdevrych@gmail.com>
2025-02-05 19:03:09 +0000

    #3 add aws s3 client, add functional upload image in create product

--- src/app/admin/products/create/page.tsx
+++ src/app/admin/products/create/page.tsx
@@ -17,6 +17,12 @@ import { Controller, SubmitHandler, useForm } from "react-hook-form";
 import ProductInput from "@/interfaces/product-input";
 import { useState } from "react";
 import axios from "axios";
+import {
+  API,
+  IMAGE_FORM_DATA_KEY,
+  PRODUCT,
+  UPLOAD_IMAGE_URL,
+} from "@/constants/api-constants";
 
 export default function CreateProduct() {
   const {
@@ -34,17 +40,37 @@ export default function CreateProduct() {
 
   const [file, setFile] = useState<File | null>(null);
   const [createError, setCreateError] = useState(false);
+  const [imageError, setImageError] = useState(false);
   const [createSuccess, setCreateSuccess] = useState(false);
 
-  const onSubmit: SubmitHandler<ProductInput> = (data: ProductInput) => {
+  const onSubmit: SubmitHandler<ProductInput> = async (data: ProductInput) => {
     setCreateError(false);
     setCreateSuccess(false);
+    setImageError(false);
 
-    const url = `${process.env.NEXT_PUBLIC_PRODUCT_API_URL}/api/product`;
-    axios
-      .post(url, data)
-      .then(() => setCreateSuccess(true))
-      .catch(() => setCreateError(true));
+    if (file) {
+      const awsS3Url = process.env.NEXT_PUBLIC_AWS_S3_URL;
+      const awS3Bucker = process.env.AWS_S3_BUCKET_NAME;
+
+      const formData = new FormData();
+      formData.append(IMAGE_FORM_DATA_KEY, file);
+
+      try {
+        await axios.post(UPLOAD_IMAGE_URL, formData);
+        data.imageUrl = `${awsS3Url}/${awS3Bucker}/${file.name}`;
+      } catch {
+        setImageError(true);
+      }
+    }
+
+    const url = `${process.env.NEXT_PUBLIC_PRODUCT_API_URL}/${API}/${PRODUCT}`;
+
+    try {
+      await axios.post(url, data);
+      setCreateSuccess(true);
+    } catch {
+      setCreateError(true);
+    }
   };
 
   return (
@@ -122,6 +148,12 @@ export default function CreateProduct() {
           {createSuccess && (
             <Alert icon={<CheckIcon fontSize="inherit" />} severity="success">
               Product create successful!
+            </Alert>
+          )}
+
+          {imageError && (
+            <Alert icon={<CloseIcon fontSize="inherit" />} severity="error">
+              Error while upload image, please try leter...
             </Alert>
           )}
 
---
+++ src/constants/api-constants.ts
@@ -0,0 +1,4 @@
+export const IMAGE_FORM_DATA_KEY = "image";
+export const UPLOAD_IMAGE_URL = "/api/images";
+export const API = "api";
+export const PRODUCT = "product";
